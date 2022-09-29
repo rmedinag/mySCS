f1e28603aefae11dcac398758ec72ec1d8cede0f
Rafael Medina <rafael.medina87@gmail.com>
2022-09-29 19:43:13 +0100

    añadida ruta /

--- src/main/java/com/wa/demo/MyScsApplication.java
+++ src/main/java/com/wa/demo/MyScsApplication.java
@@ -2,6 +2,8 @@ package com.wa.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class MyScsApplication {
@@ -10,4 +12,9 @@ public class MyScsApplication {
 		SpringApplication.run(MyScsApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	return String.format("Hello %s!", name);
+	}	
+	
 }
