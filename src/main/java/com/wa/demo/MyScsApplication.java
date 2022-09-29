cd11d486700097704ecc413355c602e55fe64474
Rafael Medina <rafael.medina87@gmail.com>
2022-09-29 16:39:27 +0100

    commit inicial

---
+++ src/main/java/com/wa/demo/MyScsApplication.java
@@ -0,0 +1,13 @@
+package com.wa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyScsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyScsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wa/demo/MyScsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyScsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
